3fe72bbaccf0967202a79a0344cb412fb7d41015
FenishPatel0245 <patelfenish245@gmail.com>
2025-03-24 00:37:43 -0400

    all

---
+++ todolist/todolist_UnitTest/todolist_UnitTest.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace todolistUnitTest
+{
+	TEST_CLASS(todolistUnitTest)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
