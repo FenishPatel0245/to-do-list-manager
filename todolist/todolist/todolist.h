3aaa745edfc31359607c3f3a16065148734f047e
Darshitaa27 <Dpatel6206@conestogac.on.ca>
2025-04-05 00:43:25 -0400

    Added task structure and basic setup to store tasks

--- todolist/todolist/todolist.h
+++ todolist/todolist/todolist.h
@@ -1,1 +1,20 @@
-#pragma once
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+
+#define MAX_TASKS 100     // Maximum number of tasks that can be stored
+#define FILENAME "tasks.txt" // File to store created tasks
+
+// Task structure
+typedef struct{
+	char description[100];  // Task description 
+    char date[11];      // YYYY-MM-DD
+    char time[6];       // HH:MM
+	int priority;      // Task priority 
+	int done;         // indicates if the task is done or not
+} Task;
+
+
