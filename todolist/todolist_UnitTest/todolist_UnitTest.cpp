af99cfa82ac4cfee6ef5a7267175f4dc340e8f01
Darshitaa27 <Dpatel6206@conestogac.on.ca>
2025-04-10 00:39:16 -0400

    Testing and adding ValidateDate_InvalidFormat() to cover invalid date scenarios.

--- todolist/todolist_UnitTest/todolist_UnitTest.cpp
+++ todolist/todolist_UnitTest/todolist_UnitTest.cpp
@@ -20,5 +20,19 @@ namespace todolistUnitTest
 			Assert::IsTrue(validate_date("2000-01-01"));
 			Assert::IsTrue(validate_date("2100-12-31"));
 		}
+
+		TEST_METHOD(ValidateDate_InvalidFormat)
+		{
+			Assert::IsFalse(validate_date("2025-4-9"));
+			Assert::IsFalse(validate_date("2025-04/09"));
+			Assert::IsFalse(validate_date("2025.04.09"));
+			Assert::IsFalse(validate_date("2025*04*09"));
+			Assert::IsFalse(validate_date("20a5-04-09"));
+			Assert::IsFalse(validate_date("2025-13-09"));
+			Assert::IsFalse(validate_date("2025-04-32"));
+			Assert::IsFalse(validate_date("1999-12-31"));
+			Assert::IsFalse(validate_date("2101-01-01"));
+			Assert::IsFalse(validate_date(""));
+		}
 	};
 }
